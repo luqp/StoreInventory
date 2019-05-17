dfec851ecc2d15d71e6d123b69e917a3fdc0bb06
windyludev <windyludev@outlook.com>
2019-05-17 01:28:16 -0400

    Cleaning the code

--- databases/inventory.py
+++ databases/inventory.py
@@ -1,8 +1,6 @@
 from peewee import *
 from datetime import datetime
 
-import csv
-
 
 db = SqliteDatabase('inventory.db')
 
@@ -16,50 +14,6 @@ class Inventory(Model):
 
     class Meta:
         database = db
-
-
-def update_item(inventory, item, date=None):
-    if not date:
-        date = item['date_updated']
-    for row in inventory:
-        if  date > row.date_updated:
-            row.product_quantity = item['product_quantity']
-            row.product_price = item['product_price']
-            row.date_updated = date
-            row.save()
-    return True
-
-
-def optain_csv_data(file='databases/inventory.csv'):
-    inventory = []
-    with open(file, newline='') as csvfile:
-        invreader = csv.DictReader(csvfile)
-        for row in list(invreader)[:]:
-            quantity = int(row['product_quantity'])
-            value = round(float(row['product_price'].split("$")[-1]) * 100.00)
-            date = datetime.strptime(row['date_updated'], "%m/%d/%Y")
-            item = {}
-            item['product_name'] = row['product_name']
-            item['product_price'] = value
-            item['product_quantity'] = quantity
-            item['date_updated'] = date
-            inventory.append(item)
-    return inventory
-
-
-def convert_to_csv_file():
-    inventory = Inventory.select()
-    products = []
-    for row in inventory:
-        value = "$" + str(row.product_price / 100)
-        date = row.date_updated.strftime("%m/%d/%Y")
-        item = {}
-        item['product_name'] = row.product_name
-        item['product_price'] = value
-        item['product_quantity'] = row.product_quantity
-        item['date_updated'] = date
-        products.append(item)
-    return products
 
 
 if __name__ == "__main__":
